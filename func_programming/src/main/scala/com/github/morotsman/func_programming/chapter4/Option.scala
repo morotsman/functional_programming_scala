55a78f2535d185075d939bfaf8d78cf2a329d486
niklas.leopold <leopold.niklas@gmail.com>
2015-03-03 21:53:11 +0100

    Option

--- func_programming/src/main/scala/com/github/morotsman/func_programming/chapter4/Option.scala
+++ func_programming/src/main/scala/com/github/morotsman/func_programming/chapter4/Option.scala
@@ -17,7 +17,8 @@ sealed trait Option[+A]{
     case Some(v) => f(v)
   }
   
-  //def flatMap2[B](f: A => Option[B]): Option[B] =
+  def flatMap2[B](f: A => Option[B]): Option[B] =
+    this.map(f).getOrElse(None)
     
   
   def orElse[B >: A](ob: => Option[B]): Option[B] = this match {
@@ -25,14 +26,20 @@ sealed trait Option[+A]{
     case Some(_) => this
   }
   
+  def orElse2[B >: A](ob: => Option[B]): Option[B] =
+    this.map(Some(_)).getOrElse(ob)
+  
   def filter(f: A => Boolean): Option[A] = this match {
-    case None => None
     case Some(v) if f(v) => this
     case _ => None
   }
-
+  
+  def filter2(f: A => Boolean): Option[A] =
+    this.map(v => if(f(v)) Some(v) else None).getOrElse(None)
     
-    
+  def filter3(f: A => Boolean): Option[A] =
+    this.flatMap(v => if(f(v)) Some(v) else None)   
+     
 }
 
 case class Some[+A](get: A) extends Option[A]
@@ -42,8 +49,22 @@ case object None extends Option[Nothing]
 
 object Option {
   
-
+  def lift[A,B](f: A => B): Option[A] => Option[B] = _ map f
   
+  def Try[A](a: => A): Option[A] = 
+    try Some(a)
+    catch { case e : Exception => None}
+  
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A,B) => C): Option[C] = {
+    a.flatMap(v1 => b.map(v2 => f(v1,v2)))
+  }
+  
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = 
+    a.foldRight(Some(Nil: List[A]): Option[List[A]])((v,acc) => acc.flatMap(acc1 => v.map(v1 => v1::acc1)))
+    
+  def traverse[A,B](a: List[A])(f: A => Option[B]): Option[List[B]] = 
+    a.foldRight(Some(Nil: List[B]): Option[List[B]])((v,acc) => acc.flatMap(acc1 => f(v).map(v1 => v1::acc1)))
+    
   
 
 }
--- func_programming/src/test/scala/com/github/morotsman/func_programming/chapter4/OptionSpec.scala
+++ func_programming/src/test/scala/com/github/morotsman/func_programming/chapter4/OptionSpec.scala
@@ -1,4 +1,4 @@
-package com.github.morotsman.func_programming.chapter3
+package com.github.morotsman.func_programming.chapter4
 
 import org.scalatest._
 
@@ -19,7 +19,11 @@ class OptionSpec  extends FlatSpec with Matchers{
   
   "Some(2).flatMap(v => Some(v))" should "result in Some(2)" in {
     assert(Some(2).flatMap(v => Some(v)) == Some(2))
-  }  
+  } 
+  
+  "Some(2).flatMap2(v => Some(v))" should "result in Some(2)" in {
+    assert(Some(2).flatMap2(v => Some(v)) == Some(2))
+  }    
   
   "Some(2).orElse(Some(3))" should "result in Some(2)" in {
     assert(Some(2).orElse(Some(3)) == Some(2))
@@ -29,13 +33,76 @@ class OptionSpec  extends FlatSpec with Matchers{
     assert(None.orElse(Some(3)) == Some(3))
   }  
   
+  "Some(2).orElse2(Some(3))" should "result in Some(2)" in {
+    assert(Some(2).orElse2(Some(3)) == Some(2))
+  }  
+  
+  "None.orElse2(Some(3))" should "result in Some(2)" in {
+    assert(None.orElse2(Some(3)) == Some(3))
+  }   
+  
   "Some(2).filter(_ == 2)" should "result in Some(2)" in {
     assert(Some(2).filter(_ == 2) == Some(2))
   }  
   
   "Some(2).filter(_ != 2)" should "result in Some(2)" in {
     assert(Some(2).filter(_ != 2) == None)
+  } 
+  
+  
+  "Some(2).filter2(_ == 2)" should "result in Some(2)" in {
+    assert(Some(2).filter2(_ == 2) == Some(2))
+  }  
+  
+  "Some(2).filter2(_ != 2)" should "result in Some(2)" in {
+    assert(Some(2).filter2(_ != 2) == None)
   }    
+  
+  "Some(2).filter3(_ == 2)" should "result in Some(2)" in {
+    assert(Some(2).filter3(_ == 2) == Some(2))
+  }  
+  
+  "Some(2).filter3(_ != 2)" should "result in Some(2)" in {
+    assert(Some(2).filter3(_ != 2) == None)
+  }
+  
+  def variance(xs: Seq[Double]): Option[Double] = {
+    val meanOfSeq = if(xs.length == 0) None else Some(xs.sum/xs.length)    
+    meanOfSeq.map(m => xs.map(x => math.pow(x-m,2))).map(s => s.sum/s.length)
+  }
+  
+  "map2(Some(2), Some(3))(_ + _)" should "result in Some(5)" in {
+    assert(Option.map2(Some(2), Some(3))(_ + _) == Some(5))
+  }  
+  
+  "map2(Some(2), None)(_ + _)" should "result in Some(5)" in {
+    assert(Option.map2(Some(2), None)(_ + _) == None)
+  }  
+  
+  "map2(None, Some(3))(_ + _)" should "result in Some(5)" in {
+    assert(Option.map2(None : Option[Int], Some(3))(_ + _) == None)
+  } 
+  
+  "sequence(List(Some(2), Some(3), Some(4)))" should "result in Some(List(2,3,4)" in {
+    assert(Option.sequence(List(Some(2), Some(3), Some(4))) == Some(List(2,3,4)))
+  } 
+  
+  "sequence(List(None, Some(3), Some(4)))" should "result in None" in {
+    assert(Option.sequence(List(None, Some(3), Some(4))) == None)
+  }   
+  
+  "sequence(List(Some(2), Some(3), None))" should "result in None" in {
+    assert(Option.sequence(List(Some(2), Some(3), None)) == None)
+  }   
+  
+  "traverse(List(2, 3, 4))(a => if(a==5) None else Some(a) )" should "result in Some(List(2,3,4))" in {
+    assert(Option.traverse(List(2, 3, 4))(a => if(a==5) None else Some(a) ) == Some(List(2,3,4)))
+  }   
+  
+  "traverse(List(2, 5, 4))(a => if(a==5) None else Some(a) )" should "result in None" in {
+    assert(Option.traverse(List(2, 5, 4))(a => if(a==5) None else Some(a) ) == None)
+  }    
+    
   
  
 
