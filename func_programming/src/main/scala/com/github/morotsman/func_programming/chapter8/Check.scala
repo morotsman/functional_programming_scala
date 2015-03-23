9a93181b8310404c660f4c2bbc7848011c5b35f6
niklas.leopold <leopold.niklas@gmail.com>
2015-03-23 21:49:31 +0100

    check

--- func_programming/src/main/scala/com/github/morotsman/func_programming/chapter8/Check.scala
+++ func_programming/src/main/scala/com/github/morotsman/func_programming/chapter8/Check.scala
@@ -26,13 +26,37 @@ object Prop {
 //State:  S => (A, S)
 case class Gen[+A](sample: State[RNG, A]) {
 
+  def map[B](f: A => B): Gen[B] = Gen {
+    this.sample.map(f)
+  }
+
+  def map2[B, C](gb: Gen[B])(f: (A, B) => C): Gen[C] = Gen {
+    this.sample.flatMap { a => gb.sample.map { b => f(a, b) } }
+  }
+
+  def genOption(): Gen[Option[A]] =
+    this.map(a => if (a == null) None else Some(a))
+
+  def flatMap[B](f: A => Gen[B]): Gen[B] =
+    Gen {
+      this.sample.flatMap(a => f(a).sample)
+    }
+
+  def listOfN(size: Gen[Int]): Gen[List[A]] =
+    size.flatMap { s => 
+      Gen {
+        State.sequence(List.fill(s)(this.sample))
+      }
+    }
+  
+
+    
+
 }
 
 object Gen {
 
   def listOf[A](a: Gen[A]): Gen[List[A]] = ???
-
-  def listOfN[A](n: Int, a: Gen[A]): Gen[List[A]] = ???
 
   def forAll[A](a: Gen[A])(f: A => Boolean): Prop = ???
 
@@ -52,5 +76,33 @@ object Gen {
     Gen {
       choose(0, 2).sample.map(v => if (v == 0) true else false)
     }
+
+  def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] = Gen {
+    State.sequence(List.fill(n)(g.sample))
+  }
+
+  def chooseAPair(start: Int, stopExclusive: Int): Gen[(Int, Int)] =
+    Gen {
+      State {
+        rng =>
+          {
+            val (result, rng2) = choose(start, stopExclusive).sample.run(rng)
+            val (result2, rng3) = choose(start, stopExclusive).sample.run(rng)
+            ((result, result2), rng3)
+          }
+      }
+    }
+
+  def chooseAPairInTermsOfMap2(start: Int, stopExclusive: Int): Gen[(Int, Int)] =
+    choose(start, stopExclusive).map2(choose(start, stopExclusive))((a, b) => (a, b))
+    
+  def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] = 
+    choose(0,2).flatMap(v => if(v == 0) g1 else g2)
+    
+  def weighted[A](g1: (Gen[A], Int), g2: (Gen[A], Int)): Gen[A] = {
+    val limit = g1._2
+    choose(0, g1._2 + g2._2).flatMap { v => if(v<limit) g1._1 else g2._1 }
+  }
+    
 
 }
--- func_programming/src/test/scala/com/github/morotsman/func_programming/chapter8/CheckSpec.scala
+++ func_programming/src/test/scala/com/github/morotsman/func_programming/chapter8/CheckSpec.scala
@@ -1,7 +1,6 @@
 package com.github.morotsman.func_programming.chapter8
 
 import org.scalatest._
-import com.github.morotsman.func_programming.chapter8.Gen
 import com.github.morotsman.func_programming.chapter6.RNG
 import com.github.morotsman.func_programming.chapter6.SimpleRNG
 import com.github.morotsman.func_programming.chapter6.State
@@ -39,6 +38,9 @@ class CheckSpec  extends FlatSpec with Matchers{
     val rng: RNG = SimpleRNG(10)
     val choosers = List.fill(1000)(Gen.choose(5, 8).sample)
     val (result, rng2) = State.sequence(choosers).run(rng)
+    assert(result.exists { x => x == 5 })
+    assert(result.exists { x => x == 6 })
+    assert(result.exists { x => x == 7 })
     assert(result.forall { x => x == 5 || x == 6 || x == 7 })
   }  
   
@@ -64,8 +66,101 @@ class CheckSpec  extends FlatSpec with Matchers{
     assert(result.exists { x => x == true })
     assert(result.exists { x => x == false })
     assert(result.forall { x => x == true || x ==false })
-  }   
+  }  
+  
+  "Gen.listOfN(3, unit(42))" should "result in List(42,42,42)" in { 
+    val rng: RNG = SimpleRNG(10)
+    val (result, rng2) = Gen.listOfN(3,Gen.unit(42)).sample.run(rng)
+    assert(result == List(42,42,42))
+  }  
+  
+  "Gen.chooseAPair(4,5)" should "result in (4,4)" in { 
+    val rng: RNG = SimpleRNG(10)
+    val (result, rng2) = Gen.chooseAPair(4, 5).sample.run(rng);
+    assert(result == (4,4))
+  } 
+  
+  
+  "Gen.unit(10).map(_+10)" should "result in 20" in { 
+    val rng: RNG = SimpleRNG(10)
+    val (result, rng2) = Gen.unit(10).map(_+10).sample.run(rng);
+    assert(result == 20)
+  }  
+  
+  "Gen.unit(10).map2(Gen.unit(10))(_+_+10)" should "result in 30" in { 
+    val rng: RNG = SimpleRNG(10)
+    val (result, rng2) = Gen.unit(10).map2(Gen.unit(10))(_+_+10).sample.run(rng);
+    assert(result == 30)
+  }    
 
+  "Gen.chooseAPairInTermsOfMap2(4,5)" should "result in (4,4)" in { 
+    val rng: RNG = SimpleRNG(10)
+    val (result, rng2) = Gen.chooseAPairInTermsOfMap2(4, 5).sample.run(rng);
+    assert(result == (4,4))
+  }   
+  
+  "Gen.unit(42).flatMap { x => Gen.unit(43) }" should "result in 43" in { 
+    val rng: RNG = SimpleRNG(10)
+    val (result, rng2) = Gen.unit(42).flatMap { x => Gen.unit(43) }.sample.run(rng);
+    assert(result == 43)
+  }    
+  
+  "Gen.choose(3, 6).listOfN(Gen.unit(1000))" should "result in a list of random 3, 5 and 6's" in { 
+    val rng: RNG = SimpleRNG(10)
+    val (result, rng2) = Gen.choose(3, 6).listOfN(Gen.unit(1000)).sample.run(rng);
+    assert(result.size == 1000)
+    assert(result.exists { x => x == 3 })
+    assert(result.exists { x => x == 4 })
+    assert(result.exists { x => x == 5 })
+    assert(result.forall { x => x == 3 || x == 4 || x == 5 })
+  }    
+  
+  "Gen.union(Gen.unit(1), Gen.unit(2)).listOfN(Gen.unit(1000))" should "result in a list of random 1 and 2's" in { 
+    val rng: RNG = SimpleRNG(10)
+    val (result, rng2) = Gen.union(Gen.unit(1), Gen.unit(2)).listOfN(Gen.unit(1000)).sample.run(rng)
+    assert(result.size == 1000)
+    assert(result.exists { x => x == 1 })
+    assert(result.exists { x => x == 2 })
+    assert(result.forall { x => x == 1 || x == 2 })
+    assert(result.filter(_ == 1).size == 496)//should be near 500
+  } 
+  
+  "Gen.weighted((Gen.unit(1),1), (Gen.unit(2),1)).listOfN(Gen.unit(1000))" should "result in a list of random 1 and 2's" in { 
+    val rng: RNG = SimpleRNG(10)
+    val (result, rng2) = Gen.weighted((Gen.unit(1),1), (Gen.unit(2),1)).listOfN(Gen.unit(1000)).sample.run(rng)
+    assert(result.size == 1000)
+    assert(result.exists { x => x == 1 })
+    assert(result.exists { x => x == 2 })
+    assert(result.forall { x => x == 1 || x == 2 })
+    assert(result.filter(_ == 1).size == 496)//should be near 500
+  } 
+  
+  "Gen.weighted((Gen.unit(1),0), (Gen.unit(2),1)).listOfN(Gen.unit(1000))" should "result in a list of 2's" in { 
+    val rng: RNG = SimpleRNG(10)
+    val (result, rng2) = Gen.weighted((Gen.unit(1),0), (Gen.unit(2),1)).listOfN(Gen.unit(1000)).sample.run(rng)
+    assert(result.size == 1000)
+    assert(result.forall { x => x == 2 })
+  }
+  
+  "Gen.weighted((Gen.unit(1),1), (Gen.unit(2),0)).listOfN(Gen.unit(1000))" should "result in a list of 2's" in { 
+    val rng: RNG = SimpleRNG(10)
+    val (result, rng2) = Gen.weighted((Gen.unit(1),1), (Gen.unit(2),0)).listOfN(Gen.unit(1000)).sample.run(rng)
+    assert(result.size == 1000)
+    assert(result.forall { x => x == 1 })
+  } 
+  
+  "Gen.weighted((Gen.unit(1),1), (Gen.unit(2),3)).listOfN(Gen.unit(1000))" should "result in a list of random 1 and 2's" in { 
+    val rng: RNG = SimpleRNG(10)
+    val (result, rng2) = Gen.weighted((Gen.unit(1),1), (Gen.unit(2),3)).listOfN(Gen.unit(1000)).sample.run(rng)
+    assert(result.size == 1000)
+    assert(result.exists { x => x == 1 })
+    assert(result.exists { x => x == 2 })
+    assert(result.forall { x => x == 1 || x == 2 })
+    assert(result.filter(_ == 1).size == 248)//should be near 250
+  }   
+  
+  
+  
   
   
   
