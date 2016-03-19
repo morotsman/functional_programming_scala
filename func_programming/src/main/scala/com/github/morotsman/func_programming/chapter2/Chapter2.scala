73c3c02736ffed91011a80334cd9c85606aa9392
niklas.leopold <leopold.niklas@gmail.com>
2016-03-19 09:55:05 +0100

    started over with the excericises

--- func_programming/src/main/scala/com/github/morotsman/func_programming/chapter2/Chapter2.scala
+++ func_programming/src/main/scala/com/github/morotsman/func_programming/chapter2/Chapter2.scala
@@ -2,38 +2,40 @@ package com.github.morotsman.func_programming.chapter2
 
 object Chapter2 {
   
-  def fib(n: Int): Int = {
-    
-    def go(n: Int): Int = n match{
-      case 1 => 0
-      case 2 => 1 
-      case _ => go(n-1) + go(n-2)
-    }
-    
-    go(n)
-  }
+  def fib(n: Int): Int = 
+    if (n == 0 || n==1){
+      0
+    }else if(n == 2){
+      1
+    } else  {
+      fib(n-1) + fib(n-2)
+    }    
+  
   
   def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
-     def go(i: Int): Boolean = {
-       if( i >= as.length - 1) true
-       else if (ordered(as(i), as(i+1))) go(i+1)
-       else false    
-     } 
      
-     go(0)
+    def go(n:Int) : Boolean =  {
+      if (n > as.length-2) {
+        true
+      }else if(!ordered (as(n), as(n+1))){
+        false
+      } else{
+        go(n+1)
+      }
+      
+    }
+    
+    go(0)   
   }
   
-  def curry[A,B,C](f: (A, B) => C): A => (B => C) = {
+  def curry[A,B,C](f: (A, B) => C): A => (B => C) = 
     (a:A) => (b:B) => f(a,b)
-  }
   
-  def uncurry[A,B,C](f: A => B => C): (A, B) => C = {
-    (a: A, b: B) => f(a)(b)
-  }
-  
-  def compose[A,B,C](f: B => C, g: A => B): A => C = {
-    (a: A) => f(g(a))  
-  }
+  def uncurry[A,B,C](f: A => B => C): (A, B) => C = 
+    (a:A, b:B) => f(a)(b)
+    
+  def compose[A,B,C](f: B => C, g: A => B): A => C = 
+    (a:A) => f(g(a))
   
 
 }
