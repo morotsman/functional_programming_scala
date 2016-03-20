1499179385104b40102d47f4c232a0fb83daf0e2
niklas.leopold <leopold.niklas@gmail.com>
2016-03-20 08:26:13 +0100

    started over with the excericises

--- func_programming/src/main/scala/com/github/morotsman/func_programming/chapter3/List.scala
+++ func_programming/src/main/scala/com/github/morotsman/func_programming/chapter3/List.scala
@@ -3,125 +3,132 @@ package com.github.morotsman.func_programming.chapter3
 sealed trait List[+A] {
 
   def tail(): List[A] = this match {
-    case Nil          => Nil
-    case Cons(hd, tl) => tl
+    case Nil => Nil
+    case Cons(x,xs) => xs
   }
 
   @annotation.tailrec
   final def drop(n: Int): List[A] = this match {
-    case Nil          => this
-    case Cons(hd, tl) => tl.drop(n - 1)
+    case Nil => Nil
+    case Cons(x,xs) if n == 1 => xs 
+    case Cons(x,xs) => xs.drop(n-1)
   }
+    
 
   @annotation.tailrec
   final def dropWhile(f: A => Boolean): List[A] = this match {
-    case Nil                   => Nil
-    case Cons(hd, tl) if f(hd) => tl.dropWhile(f);
-    case _                     => this
+    case Nil => Nil
+    case Cons(x,xs) if f(x) => xs.dropWhile(f)
+    case _ => this
   }
 
-  def append[B >: A](a2: List[B]): List[B] = this match {
+  final def append[B >: A](a2: List[B]): List[B] = this match {
     case Nil          => a2
     case Cons(hd, tl) => Cons(hd, tl.append(a2))
   }
 
-  def init(): List[A] = this match {
-    case Nil           => Nil
-    case Cons(hd, Nil) => Nil
-    case Cons(hd, tl)  => Cons(hd, tl.init())
-  }
+  final def init(): List[A] = this match {
+      case Nil => Nil
+      case Cons(x,Nil) => Nil
+      case Cons(x,xs) => Cons(x, xs.init)
+    }
+    
+  
 
-  def setHead[B >: A](head: B): List[B] = this match {
-    case Nil         => sys.error("not possible on empty list")
-    case Cons(_, tl) => Cons(head, tl)
-  }
+  final def setHead[B >: A](head: B): List[B] = this match {
+    case Nil => sys.error("setHead on empty list")
+    case Cons(x,xs) => Cons(head, xs)
+  } 
 
-  def foldRight[B](z: B)(f: (A, B) => B): B = this match {
+  //not tail rec
+  final def foldRight[B](z: B)(f: (A, B) => B): B = this match {
     case Nil          => z
     case Cons(hd, tl) => f(hd, tl.foldRight(z)(f))
   }
 
-  def length2(): Int = this.foldRight(0)((_, acc) => acc + 1)
-  def length(): Int = this.foldLeft(0)((acc, _) => acc + 1)
+  final def length(): Int = 
+    this.foldRight(0)((a,b) => b + 1)
+  
+  final def length2(): Int = ???
+  
 
-  def foldLeft[B](z: B)(f: (B, A) => B): B = {
-    @annotation.tailrec
-    def go(as: List[A], acc: B): B = as match {
-      case Nil          => acc
-      case Cons(hd, tl) => go(tl, f(acc, hd))
-    }
-
-    go(this, z)
+  @annotation.tailrec
+  final def foldLeft[B](z: B)(implicit f: (B, A) => B): B = this match {
+    case Nil => z
+    case Cons(x,xs) => xs.foldLeft(f(z,x))
   }
-
-  def reverse(): List[A] =
-    this.foldLeft(Nil: List[A])((acc, a) => Cons(a, acc))
-
-  def foldRightInTermOfFoldLeft[B](z: B)(f: (A, B) => B): B =
-    this.reverse.foldLeft(z)((a, acc) => f(acc, a))
-
-  def foldLeftInTermOfFoldRight[B](z: B)(f: (B, A) => B): B =
-    this.reverse.foldRight(z)((acc, a) => f(a, acc))
-
-  def appendInTermsOfFoldLeft[B >: A](a2: List[B]): List[B] =
-    this.reverse.foldLeft(a2)((acc, a) => Cons(a, acc))
-
-  def map[B](f: A => B): List[B] =
-    this.reverse.foldLeft(Nil: List[B])((acc, a) => Cons(f(a), acc))
-
-  def filter(f: A => Boolean): List[A] = this match {
-    case Nil                   => Nil
-    case Cons(hd, tl) if f(hd) => Cons(hd, tl.filter(f))
-    case Cons(_, tl)           => tl.filter(f)
-  }
-
-  def filter2(f: A => Boolean): List[A] = {
-    @annotation.tailrec
-    def go(as: List[A], acc: List[A]): List[A] = as match {
-      case Nil                   => acc.reverse
-      case Cons(hd, tl) if f(hd) => go(tl, Cons(hd, acc))
-      case Cons(_, tl)           => go(tl, acc)
-    }
-
-    go(this, Nil)
-  }
-
-  def filter3(f: A => Boolean): List[A] =
-    this.reverse.foldLeft(Nil: List[A])((acc, a) => if (f(a)) Cons(a, acc) else acc)
-
-  def flatMap[B](f: A => List[B]): List[B] =
-    this.reverse.foldLeft(Nil: List[B])((acc, a) => f(a).append(acc))
-
-  def filterInTermsOfFlatMap(f: A => Boolean): List[A] =
-    this.flatMap(a => if (f(a)) List(a) else Nil)
     
-  def zipWith[B, C](ls: List[B])(implicit f: (A,B) => C): List[C] = (this, ls) match {
-    case (Nil, _) => Nil
-    case (_, Nil) => Nil
-    case (Cons(hd1,tl1), Cons(hd2,tl2)) => Cons(f(hd1,hd2), tl1.zipWith(tl2))
+
+  final def reverse(): List[A] = 
+    this.foldLeft(List[A]())((b,a) => Cons(a,b))
+
+  final def foldRightInTermOfFoldLeft[B](z: B)(f: (A, B) => B): B =    
+    this.reverse.foldLeft(z)((b,a) => f(a,b))
+  
+   
+  final def foldLeftInTermOfFoldRight[B](z: B)(f: (B, A) => B): B = 
+    this.reverse.foldRight(z)((b,a) => f(a,b))
+
+  final def appendInTermsOfFoldLeft[B >: A](bl: List[B]): List[B] = 
+    this.reverse.foldLeft(bl)((b,a) => Cons(a,b))
+
+  final def map[B](f: A => B): List[B] = this match {
+    case Nil => Nil
+    case Cons(x,xs) => Cons(f(x), xs.map(f))
+  }
+    
+  final def mapInTermsOfFoldLeft[B](f: A => B): List[B] = 
+    this.reverse.foldLeft(List[B]())((acc,v) => Cons(f(v), acc))
+
+  final def filter(f: A => Boolean): List[A] = this match {
+    case Nil => Nil
+    case Cons(x,xs) if f(x) => Cons(x, xs.filter(f))
+    case Cons(x,xs) => xs.filter(f)
   }
   
-  def hasSubsequence[B >: A](sub: List[B]): Boolean = {
+  final def filterInTermsOfFoldLeft(f: A => Boolean): List[A] =
+    this.reverse.foldLeft(List[A]())((acc,v) => {
+      if(f(v)) Cons(v,acc)
+      else acc 
+    })
     
-    if(this == Nil && sub == Nil) return true
-    
-    def startsWith(ls: List[A], sub: List[B]): Boolean = (ls, sub) match{
-      case (Nil, Nil) => true
-      case (Nil,_) => false
-      case (_, Nil) => true
-      case (Cons(hd1,tl1), Cons(hd2,tl2)) if hd1==hd2 => startsWith(tl1,tl2) 
-      case _ => false
-    }
-    
-    def loop(ls: List[A]): Boolean = ls match {
-      case Nil => false
-      case Cons(_, _) if startsWith(ls, sub) => true
-      case Cons(_, tl) => loop(tl)
-    }
-    
-    loop(this)
-    
+
+  final def flatMap[B](f: A => List[B]): List[B] = {
+    val mapped = this.mapInTermsOfFoldLeft(f)
+    List.concat(mapped)
   }
+    
+
+  final def filterInTermsOfFlatMap(f: A => Boolean): List[A] =
+    this.flatMap( a => {
+      if(f(a)){
+        List(a)
+      }else {
+        Nil
+      }
+    })
+    
+  final def zipWith[B, C](ls: List[B])(implicit f: (A,B) => C): List[C] = (this,ls) match {
+    case (Nil,_) => Nil
+    case (_,Nil) => Nil
+    case (Cons(x,xs), Cons(y,ys)) => Cons(f(x,y), xs.zipWith(ys))
+  }  
+  
+  final def startsWith[B >: A](sub: List[B]): Boolean = (this,sub) match {
+    case (_,Nil) => true  
+    case (Nil,_) => false  
+    case (Cons(x,_), Cons(y,_)) if(x!=y)=> false         
+    case (Cons(x,xs), Cons(y,ys)) if (x == y) => xs.startsWith(ys)  
+  }
+  
+  
+  final def hasSubsequence[B >: A](sub: List[B]): Boolean = (this,sub) match {
+    case (Nil, Cons(y,ys)) => false
+    case _ if this.startsWith(sub) => true  
+    case (Cons(x,xs),_) => xs.hasSubsequence(sub) 
+  }
+  
+  
  
 
 }
@@ -146,16 +153,16 @@ object List {
     case Cons(hd, tl) => hd * sum(tl)
   }
 
-  def product2(ints: List[Int]): Int =
-    ints.foldRight(0)(_ * _)
+  def product2(ints: List[Int]): Int = 
+    ints.foldRight(1)(_ * _)
 
-  def sum2(ints: List[Int]): Int =
-    ints.foldLeft(0)(_ + _)
+  def sum2(ints: List[Int]): Int = 
+    ints.foldRight(1)(_ + _)
 
   def product3(ints: List[Int]): Int =
     ints.foldLeft(1)(_ * _)
 
-  def concat[A](l: List[List[A]]): List[A] =
-    l.foldLeft(Nil: List[A])((acc, a) => acc.append(a))
+  def concat[A](l: List[List[A]]): List[A] = 
+    l.foldLeft(List[A]())((acc,a) => acc.append(a))
 
 }
--- func_programming/src/test/scala/com/github/morotsman/func_programming/chapter3/ListSpec.scala
+++ func_programming/src/test/scala/com/github/morotsman/func_programming/chapter3/ListSpec.scala
@@ -39,6 +39,11 @@ class ListSpec extends FlatSpec with Matchers {
     assert(List(1,2).tail() == List(2))
   } 
   
+  "List(1,2,3).tail()" should "result in List(2,3)" in {
+    assert(List(1,2,3).tail() == List(2,3))
+  }   
+  
+  
   "List(1,2).setHead(4)" should "result in List(4,2)" in {
     assert(List(1,2).setHead(4) == List(4,2))
   }    
@@ -49,6 +54,7 @@ class ListSpec extends FlatSpec with Matchers {
     assert(listOfFruit == List(Orange(),Apple()))
   }    
   
+  
   "List().drop(1)" should "result in Nil" in {
     assert(List().drop(1) == Nil)
   }  
@@ -58,8 +64,12 @@ class ListSpec extends FlatSpec with Matchers {
   }    
   
   "List(1,2,3).drop(2)" should "result in List(3)" in {
-    assert(List(1,2,3).drop(3) == Nil)
+    assert(List(1,2,3).drop(2) == List(3))
   }  
+  
+  "List().dropWhile(_<3)" should "result in Nil" in {
+    assert(List().dropWhile((x:Int) => x < 3) == Nil)
+  }    
   
   "List(1).dropWhile(_<3)" should "result in Nil" in {
     assert(List(1).dropWhile(_ < 3) == Nil)
@@ -114,21 +124,25 @@ class ListSpec extends FlatSpec with Matchers {
     assert(List(1,2,3).foldRight(Nil:List[Int])(Cons(_,_)) == List(1,2,3))
   }   
   
-  "List(1,2,3).foldRightInTermOfFoldLeft(0)(_ + _)" should "result in 6" in {
-    assert(List(1,2,3).foldRightInTermOfFoldLeft(0)(_ + _) == 6)
-  }   
-  
-  "List(1,2,3).foldRightInTermOfFoldLeft(Nil:List[Int])(Cons(_,_))" should "result in List(1,2,3)" in {
-    assert(List(1,2,3).foldRightInTermOfFoldLeft(Nil:List[Int])(Cons(_,_)) == List(1,2,3))
-  }     
-  
   "List().length" should "result in 0" in {
     assert(List().length == 0)
   } 
   
+  "List(2).length" should "result in 1" in {
+    assert(List(2).length == 1)
+  }   
+  
   "List(1,2,3).length" should "result in 3" in {
     assert(List(1,2,3).length == 3)
-  }  
+  }   
+  
+  "List().foldLeft(0)(_ + _)" should "result in 0" in {
+    assert(List().foldLeft(0)((x: Int,y:Int) => x + y) == 0)
+  }    
+  
+  "List(2).foldLeft(0)(_ + _)" should "result in 2" in {
+    assert(List(2).foldLeft(0)(_ + _) == 2)
+  }    
   
   "List(1,2,3).foldLeft(0)(_ + _)" should "result in 6" in {
     assert(List(1,2,3).foldLeft(0)(_ + _) == 6)
@@ -138,22 +152,49 @@ class ListSpec extends FlatSpec with Matchers {
     assert(List(1,2,3).foldLeft(Nil:List[Int])((acc,a) =>Cons(a,acc)) == List(3,2,1))
   }  
   
+  "List().reverse" should "result in List()" in {
+    assert(List().reverse == List())
+  }    
+  
+  "List(1).reverse" should "result in List(1)" in {
+    assert(List(1).reverse == List(1))
+  }     
+  
+  "List(1,2,3).reverse" should "result in List(3,2,1)" in {
+    assert(List(1,2,3).reverse == List(3,2,1))
+  }   
+  
+  "List().foldRightInTermOfFoldLeft(0)(_ + _)" should "result in 0" in {
+    assert(List().foldRightInTermOfFoldLeft(0)((x:Int,y:Int) => x + y) == 0)
+  }  
+  
+  "List(2).foldRightInTermOfFoldLeft(0)(_ + _)" should "result in 2" in {
+    assert(List(2).foldRightInTermOfFoldLeft(0)(_ + _) == 2)
+  } 
+  
+  "List(1,2,3).foldRightInTermOfFoldLeft(0)(_ + _)" should "result in 6" in {
+    assert(List(1,2,3).foldRightInTermOfFoldLeft(0)(_ + _) == 6)
+  }   
+  
+  "List(1,2,3).foldRightInTermOfFoldLeft(Nil:List[Int])(Cons(_,_))" should "result in List(1,2,3)" in {
+    assert(List(1,2,3).foldRightInTermOfFoldLeft(Nil:List[Int])(Cons(_,_)) == List(1,2,3))
+  }  
+  
+  "List().foldLeftInTermOfFoldRight(0)(_ + _)" should "result in 0" in {
+    assert(List(1,2,3).foldLeftInTermOfFoldRight(0)(_ + _) == 6)
+  } 
+  
+  "List(2).foldLeftInTermOfFoldRight(0)(_ + _)" should "result in 2" in {
+    assert(List(2).foldLeftInTermOfFoldRight(0)(_ + _) == 2)
+  }   
+  
   "List(1,2,3).foldLeftInTermOfFoldRight(0)(_ + _)" should "result in 6" in {
     assert(List(1,2,3).foldLeftInTermOfFoldRight(0)(_ + _) == 6)
   }   
   
   "List(1,2,3).foldLeftInTermOfFoldRight(Nil:List[Int])(Cons(_,_))" should "result in List(3,2,1)" in {
     assert(List(1,2,3).foldLeftInTermOfFoldRight(Nil:List[Int])((acc,a) =>Cons(a,acc)) == List(3,2,1))
-  }    
-  
-  "List(1,2,3).reverse" should "result in List(3,2,1)" in {
-    assert(List(1,2,3).reverse == List(3,2,1))
   }   
-  
-  "List().reverse" should "result in List()" in {
-    assert(List().reverse == List())
-  }  
-  
   
   "List().appendInTermsOfFoldLeft(List())" should "result in Nil" in {
     assert(List().appendInTermsOfFoldLeft(List()) == Nil)
@@ -169,9 +210,9 @@ class ListSpec extends FlatSpec with Matchers {
   
   "List(1,2).appendInTermsOfFoldLeft(List(3,4,5))" should "result in List(1,2,3,4,5)" in {
     assert(List(1,2).appendInTermsOfFoldLeft(List(3,4,5)) == List(1,2,3,4,5))
-  }   
+  }     
   
-  "List.concat(List())" should "result in List(Nil)" in {
+  "List.concat(List())" should "result in Nil" in {
     assert(List.concat(List()) == Nil)
   }   
   
@@ -183,33 +224,108 @@ class ListSpec extends FlatSpec with Matchers {
     assert(List.concat(List(List(1,2),List(3,4))) == List(1,2,3,4))
   } 
   
-  "List(1,2).map(_+10)" should "result in List(11,12)" in {
-    assert(List(1,2).map(_ + 10) == List(11,12))
+  "List.concat(List(List(1,2), List(3,4),List(5,6))" should "result in List(1,2,3,4,5,6)" in {
+    assert(List.concat(List(List(1,2),List(3,4),List(5,6))) == List(1,2,3,4,5,6))
+  }   
+ 
+  
+  "List(0).map(_+10)" should "result in Nil" in {
+    assert(List().map((x:Int) => x + 10) == Nil)
+  }
+  
+  "List(1).map(_+10)" should "result in List(11)" in {
+    assert(List(1).map(_ + 10) == List(11))
+  }    
+ 
+  "List(1,2,3).map(_+10)" should "result in List(11,12,13)" in {
+    assert(List(1,2,3).map(_ + 10) == List(11,12,13))
   }  
   
   "List(1,2).map(_+'')" should "result in List('11','12')" in {
     assert(List(1,2).map(_ + "") == List("1","2"))
+    
   } 
+  
+  "List(0).mapInTermsOfFoldLeft(_+10)" should "result in Nil" in {
+    assert(List().mapInTermsOfFoldLeft((x:Int) => x + 10) == Nil)
+  }
+  
+  "List(1).mapInTermsOfFoldLeft(_+10)" should "result in List(11)" in {
+    assert(List(1).mapInTermsOfFoldLeft(_ + 10) == List(11))
+  }    
+ 
+  "List(1,2,3).mapInTermsOfFoldLeft(_+10)" should "result in List(11,12,13)" in {
+    assert(List(1,2,3).mapInTermsOfFoldLeft(_ + 10) == List(11,12,13))
+  }  
+  
+  "List(1,2).mapInTermsOfFoldLeft(_+'')" should "result in List('11','12')" in {
+    assert(List(1,2).mapInTermsOfFoldLeft(_ + "") == List("1","2")) 
+  }
+ 
+  "List().filter(_!=2)" should "result in Nil" in {
+    assert(List().filter(_ != 2) == List())
+  } 
+  
+  "List(1).filter(_!=2)" should "result in List(1)" in {
+    assert(List(1).filter(_ != 2) == List(1))
+  } 
+  
+  "List(2).filter(_!=2)" should "result in List()" in {
+    assert(List(2).filter(_ != 2) == List())
+  }   
+  
   
   "List(1,2,3).filter(_!=2)" should "result in List(1,3)" in {
     assert(List(1,2,3).filter(_ != 2) == List(1,3))
   } 
   
-  "List(1,2,3).filter2(_!=2)" should "result in List(1,3)" in {
-    assert(List(1,2,3).filter2(_ != 2) == List(1,3))
-  }     
   
-  "List(1,2,3).filter3(_!=2)" should "result in List(1,3)" in {
-    assert(List(1,2,3).filter3(_ != 2) == List(1,3))
+  "List().filterInTermsOfFoldLeft(_!=2)" should "result in Nil" in {
+    assert(List().filterInTermsOfFoldLeft(_ != 2) == List())
+  } 
+  
+  "List(1).filterInTermsOfFoldLeft(_!=2)" should "result in List(1)" in {
+    assert(List(1).filterInTermsOfFoldLeft(_ != 2) == List(1))
+  } 
+  
+  "List(2).filterInTermsOfFoldLeft(_!=2)" should "result in List()" in {
+    assert(List(2).filterInTermsOfFoldLeft(_ != 2) == List())
+  }   
+  
+  
+  "List(1,2,3).filterInTermsOfFoldLeft(_!=2)" should "result in List(1,3)" in {
+    assert(List(1,2,3).filterInTermsOfFoldLeft(_ != 2) == List(1,3))
+  }   
+  
+  "List().flatMap(a => List(a,a)" should "result in Nil" in {
+    assert(List().flatMap(a => List(a,a)) == List())
   }  
   
-  "List(1,2,3).filterInTermsOfFlatMap(_!=2)" should "result in List(1,3)" in {
-    assert(List(1,2,3).filterInTermsOfFlatMap(_ != 2) == List(1,3))
-  }     
+  "List(1).flatMap(a => List(a,a)" should "result in Lis(1,1)" in {
+    assert(List(1).flatMap(a => List(a,a)) == List(1,1))
+  }  
   
   "List(1,2,3).flatMap(a => List(a,a)" should "result in List(1,1,2,2,3,3)" in {
     assert(List(1,2,3).flatMap(a => List(a,a)) == List(1,1,2,2,3,3))
   } 
+  
+ 
+  "List().filterInTermsOfFlatMap(_!=2)" should "result in Nil" in {
+    assert(List().filterInTermsOfFlatMap(_ != 2) == List())
+  } 
+  
+  "List(1).filterInTermsOfFlatMap(_!=2)" should "result in List(1)" in {
+    assert(List(1).filterInTermsOfFlatMap(_ != 2) == List(1))
+  } 
+  
+  "List(2).filterInTermsOfFlatMap(_!=2)" should "result in List()" in {
+    assert(List(2).filterInTermsOfFlatMap(_ != 2) == List())
+  }   
+  
+  
+  "List(1,2,3).filterInTermsOfFlatMap(_!=2)" should "result in List(1,3)" in {
+    assert(List(1,2,3).filterInTermsOfFlatMap(_ != 2) == List(1,3))
+  }  
   
   "List(1,2,3).zipWith(List(1,2))(_ + _)" should "result in List(2,4)" in {
     assert(List(1,2,3).zipWith(List(1,2))(_ + _) == List(2,4))
@@ -217,11 +333,24 @@ class ListSpec extends FlatSpec with Matchers {
   
   "List(1,2).zipWith(List(1,2,3))(_ + _)" should "result in List(2,4)" in {
     assert(List(1,2).zipWith(List(1,2,3))(_ + _) == List(2,4))
-  }  
+  }    
+  
+  "List().zipWith(List(1,2,3))(_ + _)" should "result in List()" in {
+    assert(List[Int]().zipWith(List(1,2,3))(_ + _) == List())
+  }   
+  
+  "List(1,2).zipWith(List())(_ + _)" should "result in List()" in {
+    assert(List(1,2).zipWith(List[Int]())(_ + _) == List())
+  }      
+  
   
   "List().hasSubsequence(List())" should "result in true" in {
     assert(List().hasSubsequence(List()) == true)
   }  
+  
+  "List(1).hasSubsequence(List())" should "result in true" in {
+    assert(List(1).hasSubsequence(List()) == true)
+  }    
   
   "List(1).hasSubsequence(List(1))" should "result in true" in {
     assert(List(1).hasSubsequence(List(1)) == true)
@@ -243,19 +372,33 @@ class ListSpec extends FlatSpec with Matchers {
     assert(List(1,2).hasSubsequence(List(2)) == true)
   }
   
+  "List(1,2,3,4).hasSubsequence(List(1,2))" should "result in true" in {
+    assert(List(1,2,3,4).hasSubsequence(List(1,2)) == true)
+  }   
+  
   "List(1,2,3,4).hasSubsequence(List(2,3))" should "result in true" in {
     assert(List(1,2,3,4).hasSubsequence(List(2,3)) == true)
   } 
   
   "List(1,2,3,4).hasSubsequence(List(3,4))" should "result in true" in {
     assert(List(1,2,3,4).hasSubsequence(List(3,4)) == true)
-  }   
+  } 
+  
+  "List(1,2,3,4).hasSubsequence(List(1,2,3))" should "result in true" in {
+    assert(List(1,2,3,4).hasSubsequence(List(1,2,3)) == true)
+  }  
+  
+  "List(1,2,3,4).hasSubsequence(List(2,3,4))" should "result in true" in {
+    assert(List(1,2,3,4).hasSubsequence(List(2,3, 4)) == true)
+  }      
+  
+  "List(1,2,3,4).hasSubsequence(List(1,2,3,4))" should "result in true" in {
+    assert(List(1,2,3,4).hasSubsequence(List(1,2,3,4)) == true)
+  }     
   
   "List(1,2,3,4).hasSubsequence(List(2,4))" should "result in false" in {
     assert(List(1,2,3,4).hasSubsequence(List(2,4)) == false)
   }  
-  
-  
   
   
   
