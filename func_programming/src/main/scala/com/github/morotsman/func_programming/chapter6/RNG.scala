e66bb772db00a7e65ba86183bcfff6015fd29d8b
niklas.leopold <leopold.niklas@gmail.com>
2016-03-25 12:18:05 +0100

    started over with the excericises

--- func_programming/src/main/scala/com/github/morotsman/func_programming/chapter6/RNG.scala
+++ func_programming/src/main/scala/com/github/morotsman/func_programming/chapter6/RNG.scala
@@ -16,43 +16,54 @@ case class SimpleRNG(seed: Long) extends RNG {
 }
 
 object RNG {
+
   def nonNegativeInt(rng: RNG): (Int, RNG) = {
-    val (res, rng2) = rng.nextInt
-    val positiveRes = if (res < 0) Math.abs(res + 1) else res
-    (positiveRes, rng2)
+    val next = rng.nextInt
+    if (next._1 >= 0) {
+      next
+    } else {
+      nonNegativeInt(next._2)
+    }
+
   }
 
   def double(rng: RNG): (Double, RNG) = {
-    val (res, rng2) = nonNegativeInt(rng)
-    (res / (Int.MaxValue.toDouble + 1), rng2)
+    val next = nonNegativeInt(rng)
+    (next._1 / (1.0 + Int.MaxValue), next._2)
   }
 
   def intDouble(rng: RNG): ((Int, Double), RNG) = {
-    val (res, rng2) = rng.nextInt
-    val (res2, rng3) = rng2.nextInt
-    ((res, res2.toDouble), rng3)
+    val nextInt = rng.nextInt
+    val nextDouble = double(nextInt._2)
+    ((nextInt._1, nextDouble._1), nextDouble._2)
   }
 
   def doubleInt(rng: RNG): ((Double, Int), RNG) = {
-    val (res, rng2) = rng.nextInt
-    val (res2, rng3) = rng2.nextInt
-    ((res.toDouble, res2), rng3)
+    val nextDouble = double(rng)
+    val nextInt = nextDouble._2.nextInt
+    ((nextDouble._1, nextInt._1), nextInt._2)
   }
 
   def double3(rng: RNG): ((Double, Double, Double), RNG) = {
-    val (res, rng2) = rng.nextInt
-    val (res2, rng3) = rng2.nextInt
-    val (res3, rng4) = rng3.nextInt
-    ((res.toDouble, res2.toDouble, res3.toDouble), rng4)
+    val double1 = double(rng)
+    val double2 = double(double1._2)
+    val double3 = double(double2._2)
+    ((double1._1, double2._1, double3._1), double3._2)
   }
 
   def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
-    if (count == 0) (Nil, rng)
-    else {
-      val (res, rng2) = rng.nextInt
-      val (list, rng3) = ints(count - 1)(rng2)
-      (res :: list, rng3)
+
+    @annotation.tailrec
+    def go(count: Int, rng: RNG, acc: List[Int]): (List[Int], RNG) = {
+      if (count == 0) {
+        (acc, rng)
+      } else {
+        val next = rng.nextInt
+        go(count - 1, next._2, next._1 :: acc)
+      }
     }
+
+    go(count, rng, List())
   }
 
   type Rand[+A] = RNG => (A, RNG)
@@ -72,7 +83,7 @@ object RNG {
     map(nonNegativeInt)(i => i - i % 2)
 
   def doubleInTermsOfMap: Rand[Double] =
-    map(nonNegativeInt)(res => res / (Int.MaxValue.toDouble + 1))
+    map(nonNegativeInt)(v => v / (Int.MaxValue + 1))
 
   def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
     rng => {
@@ -89,48 +100,59 @@ object RNG {
   val randDoubleInt: Rand[(Double, Int)] = both(double, int)
 
   def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
-    fs.foldRight((rng: RNG) => (Nil: List[A], rng))((cur, acc) => map2(cur, acc)((v, a) => v :: a))
+    fs.foldLeft((a: RNG) => (List(): List[A], a))((acc, a) => map2(acc, a)((a, b) => b :: a))
 
-  def intsInTermsOfSequence(count: Int): Rand[List[Int]] =
-    sequence(List.fill(count)(rng => rng.nextInt))
+  def intsInTermsOfSequence(count: Int): Rand[List[Int]] = {
+    sequence(List.fill(count)(1).map { i => int })
+  }
 
   def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] =
     rng => {
-      val (res, rng2) = f(rng)
-      g(res)(rng2)
+      val (res, rng1) = map(f)(a => g(a))(rng)
+      res(rng1)
     }
 
-  def mapInTermsOfFlatMap[A, B](s: Rand[A])(f: A => B): Rand[B] =
-    flatMap(s)(a => rng => (f(a), rng))
+  def nonNegativeLessThan(n: Int): Rand[Int] =
+    flatMap(nonNegativeInt)(i => {
+      val mod = i % n
+      if (i + (n - 1) - mod >= 0) {
+        unit(mod)
+      } else {
+        println("Kommmer hit")
+        nonNegativeLessThan(n)
+      }
+    })
 
-  def map2InTermsOfFlatMap[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
-    flatMap(ra)(a => map(rb)(b => f(a, b)))
+  def mapInTermsOfFlatMap[A, B](s: Rand[A])(f: A => B): Rand[B] =
+    flatMap(s)(a => unit(f(a)))
+
+  def map2InTermsOfFlatMap[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
+    flatMap(ra)(a => flatMap(rb)(b => rng => (f(a, b), rng)))
+  }
+
 }
 
 import State._
 
 case class State[S, +A](run: S => (A, S)) {
 
-  def flatMap[B](f: A => State[S, B]): State[S, B] = {
-    State(s => {
-      val (a1, s1) = this.run(s)
-      f(a1).run(s1)
+  def flatMap[B](f: A => State[S, B]): State[S, B] =
+    State(s1 => {
+      val (a, s2) = this.run(s1)
+      f(a).run(s2)
     })
-  }
 
-  def map[B](f: A => B): State[S, B] =
-    this.flatMap(a => unit(f(a)))
+  def map[B](f: A => B): State[S, B] = {
+    this.flatMap(v => unit(f(v)))
+  }
 
 }
 
 object State {
   type Rand[A] = State[RNG, A]
 
-  def sequence[A, S](ls: List[State[S, A]]): State[S, List[A]] = {
-    ls.reverse.foldRight(State((s: S) => {
-      (Nil: List[A], s)
-    }))((cur, acc) => acc.flatMap { l => cur.map { a => a :: l } })   
-  }
+  def sequence[A, S](ls: List[State[S, A]]): State[S, List[A]] =
+    ls.foldRight(State((s: S) => (List(): List[A], s)))((acc, a) => a.flatMap(aa => acc.map(bb => bb :: aa)))
 
   def unit[A, S](a: A): State[S, A] =
     State(s => (a, s))
@@ -155,11 +177,35 @@ case object Turn extends Input
 //The state
 case class Machine(looked: Boolean, candies: Int, coins: Int)
 
-// machine => ((Int, Int), Machine)
+// machine => ((coins, candies), Machine)
 object Machine {
 
+  def stateChange(prevState: State[Machine, (Int, Int)], a: Input): State[Machine, (Int, Int)] =
+    prevState.flatMap(v =>
+      State { m =>
+        {
+          if (m.candies == 0) {
+            prevState.run(m)
+          } else if (a == Coin && m.looked) {
+            ((m.coins + 1, m.candies), Machine(false, m.candies, m.coins + 1))
+          } else if (a == Turn && !m.looked) {
+            ((m.coins, m.candies - 1), Machine(true, m.candies - 1, m.coins))
+          } else {
+            prevState.run(m)
+          }
+        }
+      })
 
-  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = {
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] =
+    State(m => {
+      val result =
+        inputs
+          .foldLeft(State((s: Machine) => ((s.coins, s.candies), s)))(stateChange)
+      result.run(m)
+    })
+    
+    /*
+  def simulateMachine2(inputs: List[Input]): State[Machine, (Int, Int)] = {
     State{ machine =>
       val listOfStateChanges = inputs.map(input =>
         State { (m:Machine) => (
@@ -173,9 +219,11 @@ object Machine {
         })
 
       val (outputs, finalMachine) = sequence(listOfStateChanges).run(machine)
+      println ("outputs: " + outputs)
       (outputs.head, finalMachine)
     } 
-  }
+  }    
   
+  */
 
 }
--- func_programming/src/test/scala/com/github/morotsman/func_programming/chapter6/RNGSpec.scala
+++ func_programming/src/test/scala/com/github/morotsman/func_programming/chapter6/RNGSpec.scala
@@ -5,27 +5,126 @@ import com.github.morotsman.func_programming.chapter6._
 
 class RNGSpec  extends FlatSpec with Matchers{
   
+  case class NotSoRandom(results: List[Int]) extends RNG {
+    
+      def nextInt: (Int, RNG) = {
+        (results.head, NotSoRandom(results.tail))
+      }
+  }
+  
   
   "SimpleRNG(1).nextInt" should "result in (384748,SimpleRNG(25214903928L))" in {
     assert(SimpleRNG(1).nextInt == (384748,SimpleRNG(25214903928L)))
   }
   
   "nonNegativeInt" should "result in non negative ints" in {
-     val (res, rng2) = SimpleRNG(1).nextInt 
-     assert(rng2.nextInt._1 < 0)
-     assert(RNG.nonNegativeInt(rng2)._1 > 0)
-  }  
+     val rng = NotSoRandom(List(-1,-2,3)) 
+     assert(rng.nextInt._1 == -1)
+     assert(RNG.nonNegativeInt(rng)._1 == 3)
+  }   
   
   "double" should "result in double" in {
-     val (res: Double, rng2: RNG) = RNG.double(SimpleRNG(1)) 
+     val rng = NotSoRandom(List(0, Int.MaxValue, Int.MaxValue/2))
+     
+     val (zero,rng1) = RNG.double(rng) 
+     assert(zero == 0)
+     
+     val (max, rng2) = RNG.double(rng1)
+     assert(max == 0.9999999995343387)
+     
+     val (half, rng3) = RNG.double(rng2)
+     assert(half == 0.4999999995343387)       
+  }  
+  
+  "intDouble" should "result in int, double" in {
+    val rng = NotSoRandom(List(Int.MaxValue, Int.MaxValue/2))
+    
+     val ((max,half),rng1) = RNG.intDouble(rng) 
+     assert(max == Int.MaxValue)
+     assert(half == 0.4999999995343387)
+  } 
+  
+  "doubleInt" should "result in int, double" in {
+    val rng = NotSoRandom(List(Int.MaxValue, Int.MaxValue/2))
+    
+     val ((max,half),rng1) = RNG.intDouble(rng) 
+     assert(max == Int.MaxValue)
+     assert(half == 0.4999999995343387)
+  }   
+   
+  "ints" should "generate a number of ints" in {
+    val rng = NotSoRandom(List(0,1,2,3,4,5))
+    
+    
+    assert(RNG.ints(0)(rng)._1 == List())
+    assert(RNG.ints(1)(rng)._1 == List(0))
+    assert(RNG.ints(6)(rng)._1 == List(0,1,2,3,4,5).reverse)
   }
   
-  "intsInTermsOfSequence" should "result in a list of ints" in {
+  "map2" should "map two rng's" in {
+    val rng1 = NotSoRandom(List(1,2))
+    val (result, nextRng) = RNG.map2(rng => rng.nextInt, rng => rng.nextInt)(_ + _) (rng1)
+    assert(result == 3)
+    
+  }
+  
+  "sequence" should "result in a list of randomnes" in {
+    val rng = NotSoRandom(List(1,2,3))
+
+    val (result1, _) = RNG.sequence(List())(rng)
+    assert(result1 == List())
+    
+    val (result2, _) = RNG.sequence(List((rng:RNG) => rng.nextInt))(rng)   
+    assert(result2 == List(1))
+    
+    val (result3, _) = RNG.sequence(List((rng:RNG) => rng.nextInt,(rng:RNG) => rng.nextInt,(rng:RNG) => rng.nextInt))(rng)   
+    assert(result3 == List(3,2,1))    
+  }
+  
+  "ints" should "generate a list of random ints" in {
+    val rng = NotSoRandom(List(1,2,3))
+    
+    assert(RNG.intsInTermsOfSequence(0)(rng)._1 == List())
+    assert(RNG.intsInTermsOfSequence(1)(rng)._1 == List(1))
+    assert(RNG.intsInTermsOfSequence(3)(rng)._1 == List(3,2,1))
+  }
+  
+  "nonNegativeLessThan" should "generate numbers below a limit" in {
+    
+    val rng = NotSoRandom(List(-1,10,5))
+    
+    assert(RNG.nonNegativeLessThan(11)(rng)._1 == 10)   
+    assert(RNG.nonNegativeLessThan(10)(rng)._1 == 0)
+    assert(RNG.nonNegativeLessThan(9)(rng)._1 == 1)
+    assert(RNG.nonNegativeLessThan(8)(rng)._1 == 2)
+      
+
+    
+  }
+  
+  "map2InTermsOfFlatMap" should "map two rng's" in {
+    val rng1 = NotSoRandom(List(1,2))
+    val (result, nextRng) = RNG.map2InTermsOfFlatMap(rng => rng.nextInt, rng => rng.nextInt)(_ + _) (rng1)
+    assert(result == 3)
+    
+  }
+    
+  
+ 
+"intsInTermsOfSequence" should "result in a list of ints" in {
      val ints = RNG.intsInTermsOfSequence(10)
      assert(ints(SimpleRNG(1))._1.length == 10)
   }     
   
   
+  "no input" should "leave machine as is" in {
+     val simulation = Machine.simulateMachine(List())
+     val result = simulation.run(Machine(true, 10, 0))
+     assert(result._1 == (0, 10))
+     assert(result._2 == Machine(true, 10, 0))
+  }
+
+
   "coin when locked" should "unlocked machine" in {
      val simulation = Machine.simulateMachine(List(Coin))
      val result = simulation.run(Machine(true, 10, 0))
@@ -47,6 +146,8 @@ class RNGSpec  extends FlatSpec with Matchers{
      assert(result._2 == Machine(true, 10, 0))
   }  
   
+  
+  
   "turn when unlocked" should "dispense candy" in {
      val simulation = Machine.simulateMachine(List(Coin, Turn))
      val result = simulation.run(Machine(true, 10, 0))
@@ -66,8 +167,7 @@ class RNGSpec  extends FlatSpec with Matchers{
      val result = simulation.run(Machine(true, 5, 10))
      assert(result._1 == (14, 1))
      assert(result._2 == Machine(true, 1, 14))
-  }   
-  
+  }  
   
   
 
